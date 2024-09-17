1fb60f268690e02da174e46dba79b28dc687459d
Alexander Polson <alex@alexpolson.com>
2024-09-17 16:23:00 -0700

    chore: Remove build scripts in favor of cargo-lambda use.

--- rust/debug.sh
+++
@@ -1,1 +0,0 @@
-cargo build && zip -j target/bootstrap.zip target/x86_64-unknown-linux-musl/debug/bootstrap
--- rust/release.sh
+++
@@ -1,1 +0,0 @@
-cargo build --release && zip -j target/bootstrap.zip target/x86_64-unknown-linux-musl/release/bootstrap
